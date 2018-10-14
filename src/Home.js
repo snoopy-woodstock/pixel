44405d299387638dbf1fa6a9e78cb8472b1f48be
Jamie Yeh <jamie.yeh@disney.com>
2018-10-13 22:53:26 -0700

    home component

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render () {
+    return (
+      <div className="home">
+        <div className='homeContainer'>
+          <div className='overlayHome'>
+            <div className='homeText'>
+              Bonjure Pixel
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
