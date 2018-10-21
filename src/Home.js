37e62db8374e04dc7dc6153bdab230b503d86861
Jamie Yeh <jamie.yeh@disney.com>
2018-10-21 09:40:56 -0700

    styles nav

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render () {
     return (
-      <div className="home">
-        Bonjure Pixel
+      <div className="home-container">
+        <div className="col-1">
+          <div className="box-1">box-1</div>
+        </div>
+        <div className="col-1">
+          <div className="box-2">box-2</div>
+        </div>
+        <div className="box-3">box-3</div>
       </div>    
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+class App extends React.Component {
+    render() {
+      return (
+        <div>  
+          <nav>
+              <ul className="nav">
+                  <li>Home</li>
+                  <li>Projects</li>
+                  <li>About</li>
+                  <li>Contact</li>
+              </ul>
+          </nav>    
+        </div>
+      );
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
